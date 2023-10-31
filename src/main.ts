ae9e4ce1519c34694d176115b8ca8ef630dbe353
ats147 <ats14795@gmail.com>
2023-10-31 18:59:38 +0530

    First commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { QrCodeModule } from './qr-code.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(QrCodeModule);
+  await app.listen(3000);
+}
+bootstrap();
